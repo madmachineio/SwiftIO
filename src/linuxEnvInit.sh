43d3645270720dc21d8993e9eb3beb05e106f54c
Andy Liu <andy@AndydeMBP.lan>
2019-06-27 18:37:15 +0800

    Add docs

---
+++ src/linuxEnvInit.sh
@@ -0,0 +1,5 @@
+export SWIFTC=/home/andy/tempInstallDir/tools/toolchains/swift/bin/swiftc
+export AR=/home/andy/tempInstallDir/tools/toolchains/gcc/bin/arm-none-eabi-ar
+export STDDIR=/home/andy/tempInstallDir/tools/toolchains/swift/lib/swift/mad/thumbv7em
+
+export INSTDIR=/home/andy/tempInstallDir/lib/SwiftIO 
---
+++ src/macEnvInit.sh
@@ -0,0 +1,5 @@
+export SWIFTC=/Users/andy/Documents/installDir/tools/toolchains/swift/bin/swiftc
+export AR=/Users/andy/Documents/installDir/tools/toolchains/gcc/bin/arm-none-eabi-ar
+export STDDIR=/Users/andy/Documents/installDir/tools/toolchains/swift/lib/swift/mad/thumbv7em
+
+export INSTDIR=/Users/andy/Documents/installDir/lib/SwiftIO 
