eee9d9e46dfe5493620a940e57723fc65ffd9b7d
madandyliu <andy@madmachine.io>
2019-08-01 17:20:32 +0800

    update

--- src/macTemp.sh
+++
@@ -1,5 +0,0 @@
-export SWIFTC=/Users/andy/Documents/installDir/tools/toolchains/swift/bin/swiftc
-export AR=/Users/andy/Documents/installDir/tools/toolchains/gcc/bin/arm-none-eabi-ar
-export STDDIR=/home/andy/macInstall/tools/toolchains/swift/lib/swift/mad/thumbv7em
-
-export INSTDIR=/home/andy/macInstall/lib/SwiftIO 
