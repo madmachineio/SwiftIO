71381ce004fc2c4afe5cecdb4ec2dffc34b072a7
Andy Liu <andy@madmachine.io>
2019-10-14 20:41:07 +0800

    update to 5.1

--- src/linuxEnvInit.sh
+++ src/linuxEnvInit.sh
@@ -1,5 +1,5 @@
-export SWIFTC=/home/andy/tempInstallDir/tools/toolchains/swift/bin/swiftc
-export AR=/home/andy/tempInstallDir/tools/toolchains/gcc/bin/arm-none-eabi-ar
-export STDDIR=/home/andy/tempInstallDir/tools/toolchains/swift/lib/swift/mad/thumbv7em
+export SWIFTC=/home/andy/swift5InstallDir/tools/toolchains/swift/bin/swiftc
+export AR=/home/andy/swift5InstallDir/tools/toolchains/gcc/bin/arm-none-eabi-ar
+export STDDIR=/home/andy/swift5InstallDir/tools/toolchains/swift/lib/swift/zephyr/thumbv7em
 
-export INSTDIR=/home/andy/tempInstallDir/lib/SwiftIO 
+export INSTDIR=/home/andy/swift5InstallDir/lib/SwiftIO 
