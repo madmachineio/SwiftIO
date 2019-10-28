4b23d46898951505f2fbc22cb846688e8efcd230
Andy Liu <andy@madmachine.io>
2019-10-28 17:32:34 +0800

    update docs

--- src/DigitalOut.swift
+++ src/DigitalOut.swift
@@ -121,3 +121,14 @@ extension DigitalOut {
     }    
 }
 
+public enum DigitalIOId: UInt8 {
+    case D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16,
+         D17, D18, D19, D20, D21, D22, D23, D24, D25, D26, D27, D28, D29, D30, D31,
+         D32, D33, D34, D35, D36, D37, D38, D39, D40, D41, D42, D43, D44, D45,
+         RED, GREEN, BLUE
+}
+
+
+public enum DigitalIODirection: UInt8 {
+    case output = 1, input
+}
