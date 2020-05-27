7cc5de81e3c30bd1e751989bec5ebdaa653490ca
Andy Liu <andy@madmachine.io>
2020-05-27 21:34:16 +0800

    Modify Nanosecond measure function
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Sources/SwiftIO/Platform.swift
+++ Sources/SwiftIO/Platform.swift
@@ -34,8 +34,11 @@ public func getPowerUpMilliseconds() -> Int64 {
 }
 
 /**
-Get the clock cycle time of the processor.
-- Returns: The time period.
+ - Attention:
+    This is the bottom layer 32bit clock drived by the CPU frequency, it would overflow evey a few seconds.
+    This function is only used to mesuare very short time interval with `cyclesToNs(start: UInt, stop: UInt)`
+Get the clock cycle of the low level 32bit timer.
+- Returns: The current clock cycle in UInt.
 */
 @inline(__always)
 public func getClockCycle() -> UInt {
@@ -43,10 +46,21 @@ public func getClockCycle() -> UInt {
 }
 
 /**
-Convert the clock cycle into nanosecond. This function is usually used together with getClockCycle.
- - Returns: The time in nanosecond after the calculation.
+
+Convert the clock cycle into nanosecond. This function is usually used together with `getClockCycle()`.
+ - Parameter start: **REQUIRED** The star cycle get by `getClockCycle()`
+ - Parameter callback: **REQUIRED** The stop cycle get by `getClockCycle()`
+ - Returns: The time in nanosecond.
 */
 @inline(__always)
-public func calcNanoseconds(_ cycles: UInt) -> UInt {
-    return UInt(swiftHal_computeNanoseconds(UInt32(cycles)))
+public func cyclesToNanosecond(start: UInt, stop: UInt) -> Int64 {
+    var cycles: UInt
+
+    if stop >= start {
+        cycles = stop - start
+    } else {
+        cycles = UInt.max - start + stop + 1
+    }
+
+    return Int64(swiftHal_computeNanoseconds(UInt32(cycles)))
 }
