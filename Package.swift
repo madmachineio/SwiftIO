006c29b1c7807e4e6f0a243c50f8d123bd8aa664
Andy Liu <andy@madmachine.io>
2021-03-25 12:39:21 +0800

    add CSwiftIO as a library
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -7,12 +7,8 @@ let package = Package(
     name: "SwiftIO",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftIO",
-            targets: ["SwiftIO"]),
-        .library(
-            name: "CSwiftIO",
-            targets: ["CSwiftIO"]),
+        .library(name: "SwiftIO", targets: ["SwiftIO"]),
+        .library(name: "CSwiftIO", targets: ["CSwiftIO"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
