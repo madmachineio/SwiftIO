2df6a12381569945ea00b4b5fc149a30bbd41b8c
Ines Zhou <ines@madmachine.io>
2021-11-05 19:17:45 +0800

    Add license

--- Sources/SwiftIO/BoardProtocol.swift
+++ Sources/SwiftIO/BoardProtocol.swift
@@ -1,3 +1,16 @@
+//=== BoardProtocol.swift -------------------------------------------------===//
+//
+// Copyright (c) MadMachine Limited
+// Licensed under MIT License
+//
+// Authors: Andy Liu
+// Created: 05/09/2021
+// Updated: 11/05/2021
+//
+// See https://madmachine.io for more information
+//
+//===----------------------------------------------------------------------===//
+
 public protocol IdName {
     var value: Int32 { get }
 }
--- Sources/SwiftIO/UART.swift
+++ Sources/SwiftIO/UART.swift
@@ -1,3 +1,16 @@
+//=== UART.swift ----------------------------------------------------------===//
+//
+// Copyright (c) MadMachine Limited
+// Licensed under MIT License
+//
+// Authors: Andy Liu
+// Created: 05/09/2021
+// Updated: 11/05/2021
+//
+// See https://madmachine.io for more information
+//
+//===----------------------------------------------------------------------===//
+
 import CSwiftIO
 
 /**
