50ce5311aee0c0833c6ab442ff443e8422b5030c
Andy Liu <andy@madmachine.io>
2022-10-13 17:19:55 +0800

    Add Network classcd
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Sources/SwiftIO/Network.swift
+++ Sources/SwiftIO/Network.swift
@@ -22,10 +22,17 @@ import CSwiftIO
 
     var mac: [UInt8]
     var txHandler: InterFaceTxHandler?
+    public var rxBuffer: [UInt8]
 
-    public init() {
+    public init(rxBufferSize: Int = 4096) {
         mac = [UInt8](repeating: 0x00, count: 6)
         txHandler = nil
+        rxBuffer = [UInt8](repeating: 0x00, count: rxBufferSize)
+    }
+
+    deinit {
+        swift_eth_tx_register(nil)
+        swift_eth_event_send(ETH_EVENT_IFACE_DISCONNECTED, nil, 0, -1)
     }
 
     public func ifConfig(mac: [UInt8], tx: InterFaceTxHandler) {
@@ -39,6 +46,20 @@ import CSwiftIO
         swift_eth_tx_register(self.txHandler!)
     }
 
+    public func ifReceive(into buffer: inout [UInt8], count: Int? = nil) {
+        var length: Int
+        if count != nil && count! < buffer.count {
+            length = count!
+        } else {
+            length = buffer.count
+        }
+
+        buffer.withUnsafeMutableBytes { pointer in
+            let ptr = pointer.baseAddress
+            swift_eth_rx(ptr, UInt16(length))
+        }
+    }
+
     public func ifUp() {
         swift_eth_event_send(ETH_EVENT_IFACE_UP, nil, 0, -1)
         swift_eth_event_send(ETH_EVENT_IFACE_CONNECTED, nil, 0, -1)
@@ -47,10 +68,4 @@ import CSwiftIO
     public func ifDown() {
         swift_eth_event_send(ETH_EVENT_IFACE_DISCONNECTED, nil, 0, -1)
     }
-
-    deinit {
-        swift_eth_tx_register(nil)
-        swift_eth_event_send(ETH_EVENT_IFACE_DISCONNECTED, nil, 0, -1)
-    }
-
  }
